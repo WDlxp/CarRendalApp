f8ee1b5c27630999ea68e3aa2095bf708052c975
WDL <18959240295@163.com>
2019-07-29 10:16:48 +0800

    添加下载图片任务的注释

--- app/src/main/java/com/example/carrendalapp/tasks/DownloadImageTask.java
+++ app/src/main/java/com/example/carrendalapp/tasks/DownloadImageTask.java
@@ -14,6 +14,9 @@ import com.example.carrendalapp.views.CircleImageView;
  * @author WD
  */
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    /**
+     * 需要设置下载下来图片的控件
+     */
     @SuppressLint("StaticFieldLeak")
     private CircleImageView civProfile;
 
@@ -23,8 +26,11 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
 
     @Override
     protected Bitmap doInBackground(String... strings) {
+        //判断参数不为空
         if (strings[0] != null) {
+            //通过BASE_URL+图片名获取图片的完整路径
             String imagePath = UrlAddress.BASE_URL + strings[0];
+            //返回下载的结果
             return ImageUtil.downloadImg(imagePath);
         }
         return null;
@@ -33,7 +39,7 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         super.onPostExecute(bitmap);
-        //下载不为空则设置头像
+        //接收下载结果不为空则设置头像
         if (bitmap != null) {
             civProfile.setImageBitmap(bitmap);
         } else {
